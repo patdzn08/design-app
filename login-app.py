5b79886a1d8fd2e69b01e2110c3ce000fcbe6eeb
patdzn <patdzn08@gmail.com>
2021-11-18 03:24:51 +0000

    edited files

--- login-app.py
+++ login-app.py
@@ -4,11 +4,11 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-sample = Flask(__name__)
+login = Flask(__name__)
 
-@sample.route("/")
+@login.route("/")
 def main():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=5050)
+    login.run(host="0.0.0.0", port=5050)
